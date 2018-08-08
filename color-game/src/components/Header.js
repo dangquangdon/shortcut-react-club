6317e671e687e9ed1a2721bf798a51b6dd9fb452
dangquangdon <38534469+dangquangdon@users.noreply.github.com>
2018-08-08 23:28:26 +0300

    Update Header.js

--- color-game/src/components/Header.js
+++ color-game/src/components/Header.js
@@ -3,10 +3,12 @@ import React from "react";
 export class Header extends React.Component {
 	render() {
 		return (
+			const box = this.props
+			const string = String(box.rgb)
 			<div className="title">
 				<h1>
 					The great RGB Color Game
-					<div id="rgb">rgb(XXX, YYY, ZZZ)</div>
+					<div id="rgb">{string}</div>
 				</h1>
 			</div>
 		);
